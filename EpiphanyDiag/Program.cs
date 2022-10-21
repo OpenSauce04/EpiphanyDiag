bc5af426ee25b696d335be114d5d7074366b4e6a
OpenSauce <opensauce04@gmail.com>
2022-10-21 15:35:31 +0100

    Added Isaac Repentance log directory checker

--- EpiphanyDiag/Program.cs
+++ EpiphanyDiag/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Formats.Tar;
+using System.IO;
+using System.Linq;
+
 namespace EpiphanyDiag
 {
 	internal class Program
@@ -7,6 +10,25 @@ namespace EpiphanyDiag
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Epiphany Diagmostics Tool v0.1 - Written by OpenSauce");
-		}
-	}
+            
+			string[] logFiles = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Documents\\My Games\\Binding of Isaac Repentance", "*.txt", SearchOption.TopDirectoryOnly);
+
+			bool validFolder = false;
+            foreach (string filePath in logFiles)
+            {
+                if (filePath.Contains("log.txt")) {
+                    validFolder = true;
+                }
+            }
+
+            if (validFolder) {
+                Console.WriteLine("Isaac Repentance log folder found.");
+			} else {
+                Console.WriteLine("ERROR: Could not find Isaac Repentance log folder!\nPress any key to exit.");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+        }
+    }
 }
