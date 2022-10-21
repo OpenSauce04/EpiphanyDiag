c924fb0f8ee01d6c7e746f6a3d92c1028e884c29
OpenSauce <opensauce04@gmail.com>
2022-10-21 14:55:33 +0100

    Initial commit

---
+++ EpiphanyDiag/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Formats.Tar;
+namespace EpiphanyDiag
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Epiphany Diagmostics Tool v0.1 - Written by OpenSauce");
+		}
+	}
+}
