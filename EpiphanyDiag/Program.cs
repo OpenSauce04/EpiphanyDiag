f254889926c7c737966ac030f08ba75921ae0617
OpenSauce04 <opensauce04@gmail.com>
2022-10-29 16:02:33 +0100

    Oops I've been using Lua too much

--- EpiphanyDiag/Program.cs
+++ EpiphanyDiag/Program.cs
@@ -14,7 +14,7 @@ namespace EpiphanyDiag
             Console.ForegroundColor = ConsoleColor.DarkRed;
 			Console.Write("Epiphany ");
             Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("Diagnostics Tool "..Strings.versionNumber.." | ");
+            Console.Write("Diagnostics Tool " + Strings.versionNumber + " | ");
             Console.ForegroundColor = ConsoleColor.DarkGray;
             Console.WriteLine("Written by OpenSauce\nSource available at: https://github.com/OpenSauce04/EpiphanyDiag\n");
             
